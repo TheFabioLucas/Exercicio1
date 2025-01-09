6383d9bc47e1baf00f4182aedd3c1a1ec8bdcd41
Fábio Lucas do Amaral Rodrigues <109112868+TheFabioLucas@users.noreply.github.com>
2025-01-09 14:07:18 -0400

    Create exercicio1.js

---
+++ exercicio1.js
@@ -0,0 +1,4 @@
+const num1 = prompt("Digite o valor aqui?");
+const num2 = prompt("Digite o valor aqui?"); 
+const resultado = parseFloat(num1) + parseFloat(num2);
+alert ("O resultado é " + resultado)
